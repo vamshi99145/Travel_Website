7fd694d50a750f6369ae4c44e5bd010dcb238140
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2021-11-26 14:27:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
@@ -8,8 +6,6 @@ let navbar = document.querySelector('.navbar');
 let imgBtn = document.querySelectorAll('.img-btn');
 
 window.onscroll = () =>{
-    searchBtn.classList.remove('fa-times');
-    searchBar.classList.remove('active');
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
     loginForm.classList.remove('active');
@@ -18,11 +14,6 @@ window.onscroll = () =>{
 menu.addEventListener('click', () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-});
-
-searchBtn.addEventListener('click', () =>{
-    searchBtn.classList.toggle('fa-times');
-    searchBar.classList.toggle('active');
 });
 
 formBtn.addEventListener('click', () =>{
